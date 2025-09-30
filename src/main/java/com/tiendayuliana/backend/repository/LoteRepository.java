26ce1f92e3f6e24e0676843dec53350f9dea00ec
Ferlandy <gaguilarc4@miumg.edu.gt>
2025-09-30 15:19:20 -0600

    feat(venta-stock): crear repositorios de venta y lotes

--- src/main/java/com/tiendayuliana/backend/repository/LoteRepository.java
+++ src/main/java/com/tiendayuliana/backend/repository/LoteRepository.java
@@ -3,4 +3,6 @@ package com.tiendayuliana.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tiendayuliana.backend.model.Lote;
-public interface LoteRepository extends JpaRepository<Lote, Integer> {}
+public interface LoteRepository extends JpaRepository<Lote, Integer> {
+    java.util.List<Lote> findByProducto_IdProductoOrderByFechaVencimientoAsc(Integer idProducto);
+}
