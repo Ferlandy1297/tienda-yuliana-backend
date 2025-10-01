23979d528b1159dff58875de4d1d8e5c115f845c
DESKTOP-IPQKNRH\neshr <erick>
2025-09-30 22:49:39 -0600

    feat(caducidad): gestionar lotes por vencer y aplicar acciones (descuento, donación, devolución)

--- src/main/java/com/tiendayuliana/backend/repository/DevolucionDetalleRepository.java
+++ src/main/java/com/tiendayuliana/backend/repository/DevolucionDetalleRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tiendayuliana.backend.model.DevolucionDetalle;
 import com.tiendayuliana.backend.model.DevolucionDetalleId;
-public interface DevolucionDetalleRepository extends JpaRepository<DevolucionDetalle, DevolucionDetalleId> {}
+public interface DevolucionDetalleRepository extends JpaRepository<DevolucionDetalle, DevolucionDetalleId> {
+    
+}
